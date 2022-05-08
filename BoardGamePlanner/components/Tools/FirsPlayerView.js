700bb9bf75aa99fd2dc07bda7c6a73baafe33f7f
L.V.M. van Loo <99014446+laurenvanloo@users.noreply.github.com>
2022-05-08 17:31:46 +0200

    adding audio and haptic feedback

--- BoardGamePlanner/components/Tools/FirsPlayerView.js
+++ BoardGamePlanner/components/Tools/FirsPlayerView.js
@@ -1,6 +1,9 @@
 import React , {useState, useMemo, useCallback, useEffect, useRef } from 'react';
 import { View, Animated, Vibration, Text, PanResponder, Dimensions, Easing} from 'react-native'
-import { globalStyles } from '../../styles'
+import { globalStyles } from '../../styles';
+import { Audio } from 'expo-av';
+import * as Haptics from 'expo-haptics';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 const gameDuration = 3000;
@@ -23,8 +26,77 @@ const FirsPlayerView = () => {
     const [timer, setTimer] = useState(0);
     const prevPlayers = usePrevPlayers(players);
     const pulseAnim = new Animated.Value(1);
-    const winnerScale = new Animated.Value(pulseAnim);
-    const [background, setBackground] = useState('white');
+    const winnerScale = new Animated.Value(1);
+    const [background, setBackground] = useState('#FCFDFC');
+    // Sound effects
+    const [touchSound, setTouchSound] = useState();
+    const [releaseSound, setReleaseSound] = useState();
+    const [winnerSound, setWinnerSound] = useState();
+
+
+    async function playTouchSound () {
+        console.log('loading sound');
+        const { sound } = await Audio.Sound.createAsync(
+            require('../../assets/Audio/Blop-Mark_DiAngelo-79054334.mp3')
+        );
+        setTouchSound(sound)
+
+        console.log("playing BLOB");
+        await sound.playAsync();
+    }
+
+    async function playReleaseSound () {
+        console.log('loading sound');
+        const { sound } = await Audio.Sound.createAsync(
+            require('../../assets/Audio/60013__qubodup__whoosh.mp3')
+        );
+        // const { sound } = await Audio.Sound.createAsync(
+        //     require('../../assets/Audio/393813__deleted-user-6479820__swish-single-2.mp3')
+        // );
+
+        setReleaseSound(sound)
+
+        console.log("playing WOOSH");
+        await sound.playAsync();
+    }
+
+    async function playWinnerSound () {
+        console.log('loading sound');
+        const { sound } = await Audio.Sound.createAsync(
+            require('../../assets/Audio/322897__rhodesmas__connected-01.mp3')
+        );
+        setWinnerSound(sound)
+
+        console.log("playing WINNER");
+        await sound.playAsync();
+    }
+
+    useEffect(()=>{
+        return touchSound ? () => 
+            {
+                console.log('unloading sound')
+                touchSound.unloadAsync();
+            }
+        : undefined
+    },[touchSound])
+
+    useEffect(()=>{
+        return releaseSound ? () => 
+            {
+                console.log('unloading sound')
+                releaseSound.unloadAsync();
+            }
+        : undefined
+    },[releaseSound])
+
+    useEffect(()=>{
+        return winnerSound ? () => 
+            {
+                console.log('unloading sound')
+                winnerSound.unloadAsync();
+            }
+        : undefined
+    },[winnerSound])
 
 
     useEffect(()=>{
@@ -32,27 +104,27 @@ const FirsPlayerView = () => {
             Animated.sequence([
                 Animated.timing(pulseAnim, {
                     toValue: 1.1,
-                    duration: 300,
+                    duration: 500,
                     easing: Easing.linear,
-                    useNativeDriver: false
+                    useNativeDriver: true
                 }),
                 Animated.timing(pulseAnim, {
                     toValue: 1.2,
-                    duration: 300,
+                    duration: 500,
                     easing: Easing.linear,
-                    useNativeDriver: false
+                    useNativeDriver: true
                 }),
                 Animated.timing(pulseAnim, {
                     toValue: 1.1,
-                    duration: 300,
+                    duration: 500,
                     easing: Easing.linear,
-                    useNativeDriver: false
+                    useNativeDriver: true
                 }),
                 Animated.timing(pulseAnim, {
                     toValue: 1.2,
-                    duration: 300,
+                    duration: 500,
                     easing: Easing.linear,
-                    useNativeDriver: false
+                    useNativeDriver: true
                 }),
             ])
         ).start()
@@ -70,14 +142,17 @@ const FirsPlayerView = () => {
     
     useEffect(()=>{
         if(timer === gameDuration){
+            Vibration.vibrate(1,1000);
             clearInterval(timerInterval);
             var tempWinner = players[Math.floor(Math.random()*players.length)];
             setWinner(tempWinner);
             setBackground(colors[tempWinner.identifier-1]);
+            playWinnerSound()
             Animated.timing(winnerScale, {
-                toValue:2.5,
-                duration: 200,
-                useNativeDriver:false
+                toValue: 2.5,
+                duration: 500,
+                easing: Easing.linear,
+                useNativeDriver:true
             })
 
             // console.log("WINNNNERERERE!!!! ",tempWinner)
@@ -99,8 +174,8 @@ const FirsPlayerView = () => {
         timerInterval = setInterval(()=>{
             // for each 100ms add 100 to the timer
             setTimer(prevTime => prevTime + 1000);
-            Vibration.vibrate(1,1000);
-        
+            
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
             console.log(timer)
         },1000)
     }
@@ -116,15 +191,15 @@ const FirsPlayerView = () => {
         console.log(winner)
         setTimer(0);
         setWinner(null);
-        setBackground('white')
+        setBackground('#FCFDFC')
     }
 
     const colors = [
-        '#4FC1E8',
-        '#A0D568',
-        '#FFCE54',
-        '#ED5564',
-        '#AC92EB',
+        '#25B4A4',
+        '#ffb100',
+        '#F194B4',
+        '#DC3030',
+        '#6359E8',
       ];
 
     // you need a value, which should transform into another value
@@ -135,7 +210,6 @@ const FirsPlayerView = () => {
     const pan = useState(new Animated.ValueXY())[0]
 
     const getPlayers = useCallback((event, gesture) => {
-        // console.log("EVENT: ", event)
         const {nativeEvent: {touches}} = event;
         const coords = touches.map(touch => ({
             identifier: touch.identifier,
@@ -163,6 +237,7 @@ const FirsPlayerView = () => {
             */
             onPanResponderGrant: () => {
                 console.log("Access granted")
+                console.log('touch detected')
                 // pulseAnim.setOffset(pulseAnim._value)
                 // it copies the value after the touch and store it so we don't lose the value when the user starts moving
                 // pan.setOffset({
@@ -177,6 +252,8 @@ const FirsPlayerView = () => {
                 // }));
             },
             onPanResponderStart:(event,gesture) => {
+                Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
+                playTouchSound();
                 getPlayers(event,gesture)
                 pulseAnim.setOffset(pulseAnim._value)
             },
@@ -192,7 +269,11 @@ const FirsPlayerView = () => {
                 // console.log("here",_.nativeEvent)
                 getPlayers(_,gesture)
             },    
-            onPanResponderEnd: (event,gesture) => getPlayers(event,gesture),
+            onPanResponderEnd: (event,gesture) => 
+                {   
+                    playReleaseSound()
+                    getPlayers(event,gesture);
+                },
 
             /* fires when the finger is released from the screen, used for clean up */
             onPanResponderRelease: () => {
@@ -228,10 +309,24 @@ const FirsPlayerView = () => {
             ]} {...panResponder.panHandlers}>
                 
                 {/* <Text style={globalStyles.h1}> {timer == 0 && players.length > 1 ? "Ready?": players.length < 1 ? "":(gameDuration - timer)/1000}</Text> */}
-
-                {players.map((player) => {
+                { players.length < 1 ? 
+                    <View style={globalStyles.container}>
+                        <Text style={[globalStyles.h4,{width:300, textAlign:'center'}]}>All players place and hold 1 finger on the screen</Text>
+                        <Text>(2 to 5 players)</Text>
+                        <View style={{
+                            display: "flex",
+                            backgroundColor:"#AFE4DE",
+                            borderRadius: 100 / 2,
+                            margin: 10
+                        }}>
+                            <MaterialCommunityIcons name="gesture-tap-hold" size={60} color='#1EA596' style={{padding: 20, alignItems: 'center',
+                            justifyContent: 'center',}}/>
+                        </View>
+                    </View> 
+                : 
+                players.map((player) => {
                     var isWinner = winner !== null && (winner.identifier == player.identifier)
-                    console.log(isWinner)
+                    // console.log(isWinner)
                     return (
                         (winner == null || isWinner) &&
                         <Animated.View
@@ -246,8 +341,8 @@ const FirsPlayerView = () => {
                                 alignItems: 'center',
                                 justifyContent: 'center',
                                 position: 'absolute',
-                                top: player.y - ((isWinner ? 225 : 130)/2),
-                                left: player.x - ((isWinner ? 225 : 130)/2),
+                                top: player.y - ((isWinner ? 225 : 130) /2),
+                                left: player.x - ((isWinner ? 225 : 130) /2),
                                 transform: [{scale: isWinner ? pulseAnim : 1}]
                             }}
                         >
