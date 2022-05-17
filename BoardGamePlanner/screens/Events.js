f9be4ab366e4a7de98138903cd8a9a11ded02569
Daniel Peña López <danielpenalopez15@gmail.com>
2022-05-17 21:52:39 +0200

    initial map region is the current location of the user

--- BoardGamePlanner/screens/Events.js
+++ BoardGamePlanner/screens/Events.js
@@ -14,7 +14,6 @@ import { Feather } from '@expo/vector-icons';
 import MapView from 'react-native-maps';
 import { Marker } from "react-native-maps";
 import Geolocation from "react-native-geolocation-service";
-import * as Permissions from 'expo-permissions';
 import * as Location from 'expo-location';
 
 
@@ -23,40 +22,56 @@ import * as Location from 'expo-location';
 
 
 export default class Events extends React.Component {
+  constructor(props){
+    super(props);
+    this.state =  {location: {
+      latitude: 42.698334,
+      longitude: 23.319941,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    }}
+  }
 
-  state = {
+/*   state = {
     location:{},
     latitude:0,
     longitude:0,
     errorMessage:''
-  }
+  } */
 
-  componentWillMount() {
+/*   componentDidMount() {
+    this._getLocation();
+  } */
+
+  componentDidMount(){
     this._getLocation();
   }
 
   _getLocation = async() =>{
-    const {status} = await Permissions.askAsync(Permissions.LOCATION);
 
-    if(status !== 'granted'){
-      console.log('PERMISSION NOT GRANTED!')
-
-      this.setState({
-        errorMessage: 'PERMISSION NOT GRANTED'
-      })
-    }
-    const location = await Location.getCurrentPositionAsync();
-
-    if(location.coords.latitude!=null){
-      this.setState({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude
-      })
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
     }
 
-    this.setState({
+    let location = await Location.getCurrentPositionAsync({}).then((result) =>{
+      {
+        this.setState({
+          location: {
+            latitude: result.coords.latitude,
+            longitude: result.coords.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          }
+        })
+    }
+    });
+
+
+    /* this.setState({
       location
-    })
+    }) */
 
     
     
@@ -70,8 +85,8 @@ export default class Events extends React.Component {
     };
     return (
       <View style={globalStyles.container}>
-      <MapView style={globalStyles.map} initialRegion={currentLocation}>
-      <Marker coordinate={currentLocation} image={require("../assets/event_img.png")}/>
+      <MapView style={globalStyles.map} initialRegion={this.state.location} region={this.state.location}>
+      <Marker coordinate={this.state.location} image={require("../assets/event_img.png")}/>
       </MapView>
         {/* <Text>{JSON.stringify(this.state.latitude)}</Text> */}
         
