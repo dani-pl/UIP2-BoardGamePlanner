61028d65612c4838571632c21f545c095e21e67c
Daniel Peña López <danielpenalopez15@gmail.com>
2022-05-31 21:54:39 +0200

    error in game library is almost fixed

--- BoardGamePlanner/screens/Profile.js
+++ BoardGamePlanner/screens/Profile.js
@@ -20,136 +20,87 @@ import {MyLibrary} from '../components/MyLibrary';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { useTranslation } from 'react-i18next';
 
+
+
 export default function Profile() {
 
-	// get the translation instance
-	const { t } = useTranslation()
+  // get the translation instance
+  const { t } = useTranslation()
 
-const navigation =useNavigation()
+  // get navigation instance
+  const navigation =useNavigation()
 
-const handleSignOut = () => {
-  auth
-  .signOut()
-            .then(()=> {navigation.replace("Login")
-            })
-         .catch(error => alert(error.message))
-}
-
-const [gameLibrary,setLibrary]=useState([])
-const [users,setUsers] =useState([])
-const [currentUser,setCurrentUser] =useState('')
-const playersRef = firebase.firestore().collection('players');
-const [modalOpen, setModalOpen] = useState(false);
-
-
-useEffect(async ()=>{
-  playersRef
-  .onSnapshot(
-  querySnapshot => {
-        const users =[]
-        querySnapshot.forEach((doc) => {
-            const {name,username,email,description, image,bggUserName,gameLibrary,
-              events,language,location,followers,following} = doc.data()
-            users.push({
-                id:doc.id,
-                name,
-                username,
-                email,
-                description,
-                image,
-                bggUserName,
-                gameLibrary,
-                events,
-                language,
-                location,
-                followers,
-                following
-            })
-        })
-    setUsers(users)
-   
-    global.gameLibrary = currentUser.gameLibrary;
- 
+  // Write Login in header when singing out
+  const handleSignOut = () => {
+    auth
+    .signOut()
+              .then(()=> {navigation.replace("Login")
+              })
+          .catch(error => alert(error.message))
   }
-)
-},[])
 
-useEffect(() => {
-for (var i = 0, len = users.length; i < len; i++) {
-if(auth.currentUser.uid == users[i].id){
-  setCurrentUser(users[i])
-  // console.log(users[i].image)
-  global.currentUser = currentUser;
-  
- 
-}
-else{
-// console.log("its not")
-};
-}
-});
+  const [currentUser,setCurrentUser] =useState('')
+  const playersRef = firebase.firestore().collection('players');
+  const [modalOpen, setModalOpen] = useState(false);
 
 
+  // Set users to the data of the users in firebase
+  const fillUsersData = () =>{
+    const something = auth.currentUser.uid
+    // set currentUser to the data of the current Use
+    const user = setCurrentUser(playersRef.where('doc.id','==', auth.currentUser.uid))
+  }
 
-
-    return (
-      <View style={globalStyles.container}>
-        <View>
-   <ProfileComponent 
- username ={currentUser.username}
-followers= {currentUser.followers}  
-following= {currentUser.following} 
-description = {currentUser.description}
- image = {currentUser.image}
-   ></ProfileComponent>
-   <View style={globalStyles.flexRowEnd}>
-
-
-  
-
-               <Pressable style={globalStyles.btnSecondary}>
+  fillUsersData()
+  return (
+  <View style={globalStyles.container}>
+    <View>
+      <ProfileComponent 
+        username ={currentUser.username}
+        followers= {currentUser.followers}  
+        following= {currentUser.following} 
+        description = {currentUser.description}
+        image = {currentUser.image}>
+      </ProfileComponent>
+      
+      <View style={globalStyles.flexRowEnd}>
+        <Pressable style={globalStyles.btnSecondary}>
           <Text style={globalStyles.btnTextNeutral}>{t('common:editBtn')}</Text>
         </Pressable>
-   <Pressable style={globalStyles.btnIconSecondary} onPress={()=>navigation.navigate("Settings", {msg: "I came from Profile"})}>
-        <FontAwesome name={'cog'} color='#2F3C3B'/>
+        <Pressable style={globalStyles.btnIconSecondary} onPress={()=>navigation.navigate("Settings", {msg: "I came from Profile"})}>
+          <FontAwesome name={'cog'} color='#2F3C3B'/>
         </Pressable>
         <Pressable style={globalStyles.btnSecondary} onPress={handleSignOut}>
           <Text style={globalStyles.btnTextNeutral}>Sign Out</Text>
         </Pressable>
-          </View>
-        </View>
+      </View>
+    </View>
   
   
-        <Text style={globalStyles.h4}>{t('common:libraryLabel')}</Text>
-        
+    <Text style={globalStyles.h4}>{t('common:libraryLabel')}</Text>
+    <MyLibrary style={globalStyles.behind} gameLibraryOftheUser={currentUser.gameLibrary}></MyLibrary>
+    <View style={globalStyles.centered}>
+      <TouchableHighlight style={globalStyles.btnPrimary}  onPress={()=>setModalOpen(true)}>
+        <Text style={globalStyles.btnTextWhite}>Add Games</Text>
+      </TouchableHighlight>
+    </View>
 
-<MyLibrary style={globalStyles.behind} index={currentUser.gameLibrary}></MyLibrary>
-<View style={globalStyles.centered}>
-<TouchableHighlight style={globalStyles.btnPrimary}  onPress={()=>setModalOpen(true)}>
-          <Text style={globalStyles.btnTextWhite}>Add Games</Text>
-        </TouchableHighlight>
-</View>
-
-<Modal visible={modalOpen} animationType='slide' style={globalStyles.modal}>
-  <View style={globalStyles.container}>
-    <FontAwesome5 name="times" size={24} style={globalStyles.modalIcon} onPress={()=> setModalOpen(false)}></FontAwesome5>
-    <Text style={globalStyles.h5}>Search for a game</Text>
-				
-				<TextInput
+    <Modal visible={modalOpen} animationType='slide' style={globalStyles.modal}>
+      <View style={globalStyles.container}>
+        <FontAwesome5 name="times" size={24} style={globalStyles.modalIcon} onPress={()=> setModalOpen(false)}></FontAwesome5>
+        <Text style={globalStyles.h5}>Search for a game</Text>
+        <TextInput
           style={globalStyles.input}
           placeholder="Search game"
-					// onChangeText='Location'
-					// value='Sofia, Bulgaria'
-				/>
-
+          // onChangeText='Location'
+          // value='Sofia, Bulgaria'
+        />
         <TouchableHighlight style={globalStyles.btnPrimary}  >
           <Text style={globalStyles.btnTextWhite}>Add Game</Text>
         </TouchableHighlight>
-
-  </View>
-</Modal>
-{/* <GameLibrary></GameLibrary> */}
       </View>
-    )
-  }
-
+    </Modal>
+    {/* <GameLibrary></GameLibrary> */}
+  </View>
+)
+};
